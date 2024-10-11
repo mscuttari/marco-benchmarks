fada962c51d6552911b89575b8c61f304264f99d
Michele Scuttari <michele.scuttari@outlook.com>
2024-10-11 16:11:46 +0200

    Keep log and results folders

--- data/scripts/run.sh
+++ data/scripts/run.sh
@@ -2,10 +2,12 @@
 
 path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
+rm -rf $BUILD_DIR
+rm -rf $LOG_DIR
+rm -rf $RESULTS_DIR
+
 "$path/marco/run.sh"
 "$path/omc/run.sh"
 
 rm -rf $BUILD_DIR
 tar -czvf /output/run-$(date +%s).tar.gz $LOG_DIR $RESULTS_DIR &> /dev/null
-rm -rf $LOG_DIR
-rm -rf $RESULTS_DIR
