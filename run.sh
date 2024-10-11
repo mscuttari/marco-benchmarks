5ff4f39fd9df810c36f9e1bda31208d942e0592d
Michele Scuttari <michele.scuttari@outlook.com>
2024-10-11 20:49:58 +0200

    Revert to run command instead of exec

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ./upgrade.sh
 ./build.sh
-docker exec -v ./data:/data -v ./marco:/tmp/marco-src -v ./output:/output marco-benchmarks /data/run.sh
+docker run --rm -v ./data:/data -v ./marco:/tmp/marco-src -v ./output:/output marco-benchmarks bash -c "/data/run.sh"
