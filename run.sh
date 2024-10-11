f3df21b37eb3040571d8721747f7a9da57149208
Michele Scuttari <michele.scuttari@outlook.com>
2024-10-11 20:45:59 +0200

    Fix exec command

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ./upgrade.sh
 ./build.sh
-docker exec --rm -v ./data:/data -v ./marco:/tmp/marco-src -v ./output:/output marco-benchmarks /data/run.sh
+docker exec -v ./data:/data -v ./marco:/tmp/marco-src -v ./output:/output marco-benchmarks /data/run.sh
