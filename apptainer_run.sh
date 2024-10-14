34c17624d58cef1d336acc86d52b30aea0396eed
Michele Scuttari <michele.scuttari@outlook.com>
2024-10-14 22:15:03 +0200

    Create output folder if missing

--- apptainer_run.sh
+++ apptainer_run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 ./apptainer_build.sh
+mkdir -p output
 apptainer exec --bind ./data:/data --bind ./marco:/tmp/marco-src --bind ./output:/output marco-benchmarks.sif bash -c /data/run.sh
