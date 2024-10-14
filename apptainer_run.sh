8defef34a6daef4804ef0ae7e89ba6714d7ba4a4
Michele Scuttari <michele.scuttari@outlook.com>
2024-10-14 22:12:15 +0200

    Add missing image name in exec command

--- apptainer_run.sh
+++ apptainer_run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 ./apptainer_build.sh
-apptainer exec --bind ./data:/data --bind ./marco:/tmp/marco-src --bind ./output:/output bash -c /data/run.sh
+apptainer exec --bind ./data:/data --bind ./marco:/tmp/marco-src --bind ./output:/output marco-benchmarks.sif bash -c /data/run.sh
