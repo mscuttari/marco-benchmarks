2b09f16be0c16019a20fc3b912a4831ef707534f
Michele Scuttari <michele.scuttari@outlook.com>
2024-10-14 22:52:53 +0200

    Add timeout environment variables to run commands

--- apptainer_run.sh
+++ apptainer_run.sh
@@ -3,4 +3,13 @@
 ./apptainer_build.sh
 mkdir -p output
 mkdir -p root
-apptainer exec --bind ./data:/data --bind ./marco:/tmp/marco-src --bind ./output:/output --bind ./root:/root marco-benchmarks.sif bash -c /data/run.sh
+
+apptainer exec \
+	--bind ./data:/data \
+	--bind ./marco:/tmp/marco-src \
+	--bind ./output:/output \
+	--bind ./root:/root \
+	--env COMPILE_TIMEOUT=1200 \
+	--env SIMULATE_TIMEOUT=3600 \
+	marco-benchmarks.sif \
+	bash -c /data/run.sh
--- docker_run.sh
+++ docker_run.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
+
 ./docker_upgrade.sh
 ./docker_build.sh
 mkdir -p output
-docker run --rm -v ./data:/data -v ./marco:/tmp/marco-src -v ./output:/output marco-benchmarks bash -c "/data/run.sh"
+
+docker run --rm \
+	-v ./data:/data \
+	-v ./marco:/tmp/marco-src \
+	-v ./output:/output \
+	-e COMPILE_TIMEOUT=1200 \
+	-e SIMULATE_TIMEOUT=3600 \
+	marco-benchmarks \
+	bash -c "/data/run.sh"
