b5c2f59246690ea044f5a22b808fa08e11903822
Michele Scuttari <michele.scuttari@outlook.com>
2024-10-14 22:15:24 +0200

    Create output folder if missing

--- docker_run.sh
+++ docker_run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 ./docker_upgrade.sh
 ./docker_build.sh
+mkdir -p output
 docker run --rm -v ./data:/data -v ./marco:/tmp/marco-src -v ./output:/output marco-benchmarks bash -c "/data/run.sh"
