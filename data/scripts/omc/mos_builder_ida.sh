2393b5a61b13d3fbaf87a1be93ffa8eda40c1a63
Michele Scuttari <michele.scuttari@outlook.com>
2024-11-18 19:26:19 +0100

    Fix mos script for IDA

--- data/scripts/omc/mos_builder_ida.sh
+++ data/scripts/omc/mos_builder_ida.sh
@@ -8,11 +8,14 @@ resultsfile=$5
 
 echo "loadFile(\"$SRC_DIR/ThermalChipOO-$nx-$ny-$nz.mo\");" > $outfile
 echo "getErrorString();" >> $outfile
+echo "setCommandLineOptions(\"--daeMode\")" >> $outfile
+echo "getErrorString();" >> $outfile
 echo "buildModel(" >> $outfile
 echo "    ThermalChipOO.Models.ThermalChipSimpleBoundary," >> $outfile
 echo "    method=\"ida\"," >> $outfile
 echo "    stopTime=0.4," >> $outfile
 echo "    outputFormat=\"csv\"," >> $outfile
 echo "    variableFilter=\"Tct1|Tct2|Tct3|Tct4|Tcb1|Tcb2|Tcb3|Tcb4\"," >> $outfile
-echo "    simflags=\"-lv=LOG_STATS_V -r=\\\"results.csv\\\"\");" >> $outfile
+echo "    cflags=\"-O0\"" >> $outfile
+echo "    simflags=\"-noEquidistantTimeGrid -lv=LOG_STATS_V -r=\\\"results.csv\\\"\");" >> $outfile
 echo "getErrorString();" >> $outfile
