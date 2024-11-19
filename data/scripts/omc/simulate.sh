c4fc113ea4defbc6a3515e1497b4d3a1e449da8c
Michele Scuttari <michele.scuttari@outlook.com>
2024-11-19 11:35:28 +0100

    Fix OMC results dir

--- data/scripts/omc/simulate.sh
+++ data/scripts/omc/simulate.sh
@@ -9,5 +9,5 @@ solver=$4
 
 TIME_FILE=$LOG_DIR/simulation-time_$nx-$ny-$nz-$solver.txt
 /usr/bin/time -p -o $TIME_FILE "$path/simulation_run.sh" $BUILD_DIR ThermalChipOO.Models.ThermalChipSimpleBoundary || exit 1
-mv "$BUILD_DIR/results.csv" "$RESULTS_DIR/omc/results-$nx-$ny-$nz.csv"
+mv "$BUILD_DIR/results.csv" "$RESULTS_DIR/results-$nx-$ny-$nz.csv"
 cat $TIME_FILE
