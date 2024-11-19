b94ea2b5dc474f3ce5427fc98af7c6dfa2270c57
Michele Scuttari <michele.scuttari@outlook.com>
2024-11-19 10:45:15 +0100

    Fix mos builder scripts

--- data/scripts/omc/mos_builder_euler-forward.sh
+++ data/scripts/omc/mos_builder_euler-forward.sh
@@ -15,6 +15,6 @@ echo "    stopTime=0.4," >> $outfile
 echo "    numberOfIntervals=266667," >> $outfile
 echo "    outputFormat=\"csv\"," >> $outfile
 echo "    variableFilter=\"Tct1|Tct2|Tct3|Tct4|Tcb1|Tcb2|Tcb3|Tcb4\"," >> $outfile
-echo "    cflags=\"-O0\"" >> $outfile
+echo "    cflags=\"-O0\"," >> $outfile
 echo "    simflags=\"-lv=LOG_STATS_V -r=\\\"results.csv\\\"\");" >> $outfile
 echo "getErrorString();" >> $outfile
