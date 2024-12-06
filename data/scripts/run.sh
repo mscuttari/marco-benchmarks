2f3f598f89163de5c2393d13ae77650102c4f6a1
Michele Scuttari <michele.scuttari@outlook.com>
2024-12-06 19:36:00 +0100

    Print MARCO and OMC versions

--- data/scripts/run.sh
+++ data/scripts/run.sh
@@ -9,5 +9,11 @@ rm -rf $RESULTS_DIR
 "$path/marco/run.sh"
 "$path/omc/run.sh"
 
+echo "MARCO version"
+marco --version
+
+echo "OMC version"
+omc --version
+
 rm -rf $BUILD_DIR
 tar -czvf /output/run-$(date +%s).tar.gz $LOG_DIR $RESULTS_DIR &> /dev/null
